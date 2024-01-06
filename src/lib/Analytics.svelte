14174dbf2fd941ba031adc966dc0c74cf5615349
nate esh <nathan.eshraghi@gmail.com>
2024-01-06 17:29:41 +1000

    install Google Analytics

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+
+    $: {
+        if (typeof gtag !== "undefined") {
+            gtag("config", "MEASUREMENT_ID", {
+                page_title: document.title,
+                page_path: $page.url.pathname,
+            });
+        }
+    }
+</script>
+
+<svelte:head>
+    <!-- Google tag (gtag.js) -->
+    <!-- Google tag (gtag.js) -->
+    <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-GGMZ4J6MNP"
+    ></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-GGMZ4J6MNP");
+    </script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
   import "../app.css";
+  import Analytics from "$lib/Analytics.svelte";
 </script>
+
+<Analytics />
 
 <slot />
