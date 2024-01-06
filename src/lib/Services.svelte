da2aa28e6e461aaf6cc39c0912efc53d65b85851
nate esh <nathan.eshraghi@gmail.com>
2024-01-07 09:25:14 +1000

    Update GIS Training and CAD software drafting

--- src/lib/Services.svelte
+++ src/lib/Services.svelte
@@ -11,14 +11,14 @@
     <ul class="service-list list-none text-lg grid xs:grid-cols-2 grid-cols-1 gap-3 whitespace-pre-wrap">
       <li>Vector and raster <span class="text-lime-700/80">Spatial analysis.</span></li>
       <li><span class="text-lime-700/80">Database management</span> (PostgreSQL, SpatiaLite).</li>
-      <li><span class="text-lime-700/80">Enterprise</span> GIS Training - ArcGIS.</li>
-      <li><span class="text-lime-700/80">Open-source</span> GIS Training - QGIS.</li>
+      <li>Enterprise GIS <span class="text-lime-700/80">Training - ArcGIS</span>.</li>
+      <li>Open-source GIS <span class="text-lime-700/80">Training - QGIS</span>.</li>
       <li>Custom <span class="text-lime-700/80">Geo-spatial programming</span> (Python).</li>
       <li><span class="text-lime-700/80">Web mapping</span> and web development.</li>
       <li><span class="text-lime-700/80">VertiGIS Studio</span> and Workflow application development.</li>
       <li>Data visualisation and <span class="text-lime-700/80">dashboards</span></li>
       <li>Data processing, <span class="text-lime-700/80">automation</span>, Supervised machine-learning.</li>
-      <li><span class="text-lime-700/80">CAD</span> software drafting (AutoCAD)</li>
+      <li><span class="text-lime-700/80">CAD</span> software drafting (AutoCAD).</li>
     </ul>
   </div>
 </div>
